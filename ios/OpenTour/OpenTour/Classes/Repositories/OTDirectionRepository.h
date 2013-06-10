c10953c3e437ad3346f5ee5103f193a3cd90091b
Matthew Teece <mteece@gmail.com>
2013-06-10 14:07:47 -0400

    Implementing AFNetworking calls that will not block the main thread.

--- ios/OpenTour/OpenTour/Classes/Repositories/OTDirectionRepository.h
+++ ios/OpenTour/OpenTour/Classes/Repositories/OTDirectionRepository.h
@@ -10,10 +10,8 @@
 
 @interface OTDirectionRepository : NSObject
 
-- (void)setDirectionsQuery:(NSDictionary *)object withSelector:(SEL)selector
-              withDelegate:(id)delegate;
+- (void)setDirectionsQuery:(NSDictionary *)object withSelector:(SEL)selector withDelegate:(id)delegate;
 - (void)retrieveDirections:(SEL)sel withDelegate:(id)delegate;
-- (void)fetchedData:(NSData *)data withSelector:(SEL)selector
-       withDelegate:(id)delegate;
+- (void)fetchedData:(NSDictionary *)data withSelector:(SEL)selector withDelegate:(id)delegate;
 
 @end
--- ios/OpenTour/OpenTour/Classes/Repositories/OTDirectionRepository.m
+++ ios/OpenTour/OpenTour/Classes/Repositories/OTDirectionRepository.m
@@ -8,6 +8,8 @@
 
 #import "OTDirectionRepository.h"
 
+#import <AFNetworking.h>
+
 @implementation OTDirectionRepository
 {
     @private
@@ -17,19 +19,17 @@
         NSArray *_waypoints;
 }
 
-static NSString *kMDDirectionsURL = @"http://maps.googleapis.com/maps/api/directions/json?";
+static NSString *kMDDirectionsURL = @"https://maps.googleapis.com/maps/api/directions/json?";
 
-- (void)setDirectionsQuery:(NSDictionary *)query withSelector:(SEL)selector
-              withDelegate:(id)delegate{
+- (void)setDirectionsQuery:(NSDictionary *)query withSelector:(SEL)selector withDelegate:(id)delegate
+{
     NSArray *waypoints = [query objectForKey:@"waypoints"];
     NSString *origin = [waypoints objectAtIndex:0];
     int waypointCount = [waypoints count];
     int destinationPos = waypointCount -1;
     NSString *destination = [waypoints objectAtIndex:destinationPos];
     NSString *sensor = [query objectForKey:@"sensor"];
-    NSMutableString *url =
-    [NSMutableString stringWithFormat:@"%@&origin=%@&destination=%@&sensor=%@&mode=walking",
-     kMDDirectionsURL,origin,destination, sensor];
+    NSMutableString *url = [NSMutableString stringWithFormat:@"%@&origin=%@&destination=%@&sensor=%@&mode=walking", kMDDirectionsURL,origin,destination, sensor];
     if(waypointCount>2) {
         [url appendString:@"&waypoints=optimize:true"];
         int wpCount = waypointCount-2;
@@ -38,28 +38,37 @@ static NSString *kMDDirectionsURL = @"http://maps.googleapis.com/maps/api/direct
             [url appendString:[waypoints objectAtIndex:i]];
         }
     }
-    url = [url
-           stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding];
-    _directionsURL = [NSURL URLWithString:url];
+    NSString *urlWithParams = [url stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+    _directionsURL = [NSURL URLWithString:urlWithParams];
     [self retrieveDirections:selector withDelegate:delegate];
 }
+
 - (void)retrieveDirections:(SEL)selector withDelegate:(id)delegate{
-    dispatch_async(dispatch_get_main_queue(), ^{
+    /*dispatch_async(dispatch_get_main_queue(), ^{
         NSData* data =
         [NSData dataWithContentsOfURL:_directionsURL];
         [self fetchedData:data withSelector:selector withDelegate:delegate];
-    });
+    });*/
+    
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:_directionsURL];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id json) {
+        NSLog(@"%@", json);
+        //NSError* error;
+        //NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:json options:kNilOptions error:&error];
+        
+        [self fetchedData:json withSelector:selector withDelegate:delegate];
+        
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"Request Failed with Error: %@, %@", error, error.userInfo);
+    }];
+    [operation start];
+    
 }
 
-- (void)fetchedData:(NSData *)data
-       withSelector:(SEL)selector
-       withDelegate:(id)delegate{
-    
-    NSError* error;
-    NSDictionary *json = [NSJSONSerialization
-                          JSONObjectWithData:data
-                          options:kNilOptions
-                          error:&error];
-    [delegate performSelector:selector withObject:json];
+- (void)fetchedData:(NSDictionary *)data withSelector:(SEL)selector withDelegate:(id)delegate
+{    
+    [delegate performSelector:selector withObject:data];
 }
 @end

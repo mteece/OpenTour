d6fa3cf7495061588e17b40d0d5a3106944a57b2
Matthew Teece <mteece@gmail.com>
2013-07-17 12:54:20 -0400

    CocoaPods update, and Google Maps for IOS Update.

---
+++ ios/OpenTour/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// Base64nl
+#define COCOAPODS_POD_AVAILABLE_Base64nl
+#define COCOAPODS_VERSION_MAJOR_Base64nl 1
+#define COCOAPODS_VERSION_MINOR_Base64nl 1
+#define COCOAPODS_VERSION_PATCH_Base64nl 0
+
+// Google-Maps-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Google_Maps_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Google_Maps_iOS_SDK 1
+#define COCOAPODS_VERSION_MINOR_Google_Maps_iOS_SDK 4
+#define COCOAPODS_VERSION_PATCH_Google_Maps_iOS_SDK 0
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 0
+
